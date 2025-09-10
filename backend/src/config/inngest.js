4988b1e5367d0b276f8d0d4c537d84b87172c83f
Lovekesh Singh <lovekeshsingh374@gmail.com>
2025-09-10 15:40:29 +0530

    import path fixed

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "../config/db.js";
-import { User } from "../models/User.js";
+import { User } from "../models/user.model.js";
 
 export const inngest = new Inngest({ id: "drovo-app" });
 
